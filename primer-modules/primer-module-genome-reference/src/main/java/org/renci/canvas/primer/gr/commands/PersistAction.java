30daafa684215f29de327f594032dac187ed5d03
Jason Reilly <jdr0887@gmail.com>
2017-04-12 14:57:35 -0400

    removing

--- primer-modules/primer-module-genome-reference/src/main/java/org/renci/canvas/primer/gr/commands/PersistAction.java
+++ primer-modules/primer-module-genome-reference/src/main/java/org/renci/canvas/primer/gr/commands/PersistAction.java
@@ -1,7 +1,14 @@
 package org.renci.canvas.primer.gr.commands;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.Executors;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.karaf.shell.api.action.Action;
 import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.Option;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.renci.canvas.dao.CANVASDAOBeanService;
@@ -9,7 +16,7 @@ import org.renci.canvas.primer.dao.PrimerDAOBeanService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Command(scope = "hgref", name = "persist", description = "Persist Human Genome Reference data")
+@Command(scope = "genome-reference", name = "persist", description = "Persist")
 @Service
 public class PersistAction implements Action {
 
@@ -29,7 +36,7 @@ public class PersistAction implements Action {
     public Object execute() throws Exception {
         logger.debug("ENTERING execute()");
 
+        Executors.newSingleThreadExecutor().submit(new PersistRunnable(canvasDAOBeanService));
         return null;
     }
-
 }
