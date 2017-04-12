4476a09f245e7787c44d9f29d63d0d196b3da2d5
Jason Reilly <jdr0887@gmail.com>
2017-04-12 15:00:45 -0400

    testing

--- primer-modules/primer-module-genome-reference/src/test/java/org/renci/canvas/primer/gr/commands/Scratch.java
+++ primer-modules/primer-module-genome-reference/src/test/java/org/renci/canvas/primer/gr/commands/Scratch.java
@@ -1,12 +1,19 @@
 package org.renci.canvas.primer.gr.commands;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
+
+import com.google.common.base.Splitter;
 
 public class Scratch {
 
@@ -31,4 +38,59 @@ public class Scratch {
         }
 
     }
+
+    @Test
+    public void test() {
+        String asdf = "GATCACAGGTCTATCACCCTATTAACCACTCACGGGAGCTCTCCATGCATTTGGTATTTTCGTCTGGGGG"
+                + "GTGTGCACGCGATAGCATTGCGAGACGCTGGAGCCGGAGCACCCTATGTCGCAGTATCTGTCTTTGATTC"
+                + "CTGCCTCATTCTATTATTTATCGCACCTACGTTCAATATTACAGGCGAACATACCTACTAAAGTGTGTTA"
+                + "ATTAATTAATGCTTGTAGGACATAATAATAACAATTGAATGTCTGCACAGCCGCTTTCCACACAGACATC";
+
+        System.out.println(StringUtils.split(asdf, null, 70)[0]);
+        System.out.println(Splitter.fixedLength(70).split(asdf).iterator().next());
+
+    }
+
+    @Test
+    public void testFileNamePattern() {
+        Pattern fileNamePattern = Pattern.compile(
+                "hs_ref_(?<genomeRefAccession>GRCh\\d+)\\.p(?<patch>\\d+)_(chr)?(?<chromosome>\\d+|MT|Un|X|Y|unlocalized|unplaced|alts)\\.fa\\.gz");
+
+        for (String file : Arrays.asList("hs_ref_GRCh38.p7_chr4.fa.gz", "hs_ref_GRCh38.p7_chrX.fa.gz", "hs_ref_GRCh38.p7_chrY.fa.gz",
+                "hs_ref_GRCh38.p7_unlocalized.fa.gz", "hs_ref_GRCh38.p7_unplaced.fa.gz")) {
+
+            Matcher m = fileNamePattern.matcher(file);
+            assertTrue(m.find());
+            System.out.println(m.group("genomeRefAccession"));
+            System.out.println(m.group("patch"));
+            System.out.println(m.group("chromosome"));
+        }
+
+    }
+
+    @Test
+    public void testDescPattern() {
+        Pattern fileNamePattern = Pattern
+                .compile("Homo sapiens chromosome (?<contig>\\d+|X|Y).+(?<genomeRefAccession>GRCh\\d+)\\.p(?<patch>\\d+).+");
+        for (String file : Arrays.asList("Homo sapiens chromosome 1 genomic patch of type FIX, GRCh38.p7 PATCHES HG1342_HG2282_PATCH",
+                "Homo sapiens chromosome X genomic scaffold, GRCh38.p7 alternate locus group ALT_REF_LOCI_2 HSCHRX_2_CTG3",
+                "Homo sapiens chromosome Y genomic patch of type FIX, GRCh38.p7 PATCHES HG2062_PATCH")) {
+            Matcher m = fileNamePattern.matcher(file);
+            assertTrue(m.find());
+            System.out.println(m.group("contig"));
+            System.out.println(m.group("genomeRefAccession"));
+            System.out.println(m.group("patch"));
+        }
+
+    }
+
+    @Test
+    public void testParseFastaKey() {
+        String asdf = ">gi|568815576|ref|NC_000022.11| Homo sapiens chromosome 22, GRCh38.p7 Primary Assembly";
+        String[] asdfSplit = asdf.split("\\|");
+        for (String a : asdfSplit) {
+            System.out.println(a);
+        }
+    }
+
 }
