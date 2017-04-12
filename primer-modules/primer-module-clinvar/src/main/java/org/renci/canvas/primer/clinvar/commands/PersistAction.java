bd44eb9978a13948ec2c7a77be81f3dfb4ea4784
Jason Reilly <jdr0887@gmail.com>
2017-04-12 15:00:20 -0400

    calling runnable

--- primer-modules/primer-module-clinvar/src/main/java/org/renci/canvas/primer/clinvar/commands/PersistAction.java
+++ primer-modules/primer-module-clinvar/src/main/java/org/renci/canvas/primer/clinvar/commands/PersistAction.java
@@ -1,22 +1,13 @@
 package org.renci.canvas.primer.clinvar.commands;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.concurrent.Executors;
 
-import org.apache.commons.lang3.time.FastDateFormat;
-import org.apache.commons.lang3.tuple.Pair;
 import org.apache.karaf.shell.api.action.Action;
 import org.apache.karaf.shell.api.action.Command;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.osgi.framework.BundleContext;
 import org.renci.canvas.dao.CANVASDAOBeanService;
-import org.renci.canvas.primer.commons.FTPFactory;
 import org.renci.canvas.primer.dao.PrimerDAOBeanService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,6 +24,9 @@ public class PersistAction implements Action {
     @Reference
     private PrimerDAOBeanService annotationDAOBeanService;
 
+    @Reference
+    private BundleContext bundleContext;
+
     public PersistAction() {
         super();
     }
@@ -41,36 +35,8 @@ public class PersistAction implements Action {
     public Object execute() throws Exception {
         logger.debug("ENTERING execute()");
 
-        String date = FastDateFormat.getInstance("yyyy-MM").format(new Date());
-
-        Path outputPath = Paths.get(System.getProperty("karaf.data"), "tmp", "clinvar");
-        File outputDir = outputPath.toFile();
-        outputDir.mkdirs();
-
-        File clinvarXmlFile = FTPFactory.ncbiDownload(outputDir, "/pub/clinvar/xml", String.format("ClinVarFullRelease_%s.xml.gz", date));
-        List<String> schemaFileList = FTPFactory.ncbiListRemoteFiles("/pub/clinvar/xml", "clinvar_public_", ".xsd");
-
-        Pattern p = Pattern.compile("clinvar_public_(?<v1>//d+)\\.(?<v2>//d+)\\.xsd");
-        List<Pair<Integer, Integer>> versionPairList = new ArrayList<>();
-        for (String schemaFile : schemaFileList) {
-            Matcher m = p.matcher(schemaFile);
-            if (m.find()) {
-                String v1 = m.group("v1");
-                String v2 = m.group("v2");
-                versionPairList.add(Pair.of(Integer.valueOf(v1), Integer.valueOf(v2)));
-            }
-        }
-
-        versionPairList.sort((a, b) -> {
-            int ret = b.getLeft().compareTo(a.getLeft());
-            if (ret == 0) {
-                ret = b.getRight().compareTo(a.getRight());
-            }
-            return ret;
-        });
-
-        File clinvarXSDFile = FTPFactory.ncbiDownload(outputDir, "/pub/clinvar/xsd_public",
-                String.format("clinvar_public_%d.%d.xsd", versionPairList.get(0).getLeft(), versionPairList.get(0).getRight()));
+        Executors.newSingleThreadExecutor().submit(new PersistUsingSequenceLocation(canvasDAOBeanService));
+        // Executors.newSingleThreadExecutor().submit(new PersistUsingHGVS(canvasDAOBeanService, bundleContext));
 
         return null;
     }
