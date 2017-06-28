4678c8065d6dd25aa6e2cde1cc9f41b197690826
Jason Reilly <jdr0887@gmail.com>
2017-06-28 12:26:27 -0400

    initial commit

---
+++ primer-commons/src/main/java/org/renci/canvas/primer/PrimerException.java
@@ -0,0 +1,23 @@
+package org.renci.canvas.primer;
+
+public class PrimerException extends Exception {
+
+    private static final long serialVersionUID = 1559994228502243354L;
+
+    public PrimerException() {
+        super();
+    }
+
+    public PrimerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PrimerException(String message) {
+        super(message);
+    }
+
+    public PrimerException(Throwable cause) {
+        super(cause);
+    }
+
+}
