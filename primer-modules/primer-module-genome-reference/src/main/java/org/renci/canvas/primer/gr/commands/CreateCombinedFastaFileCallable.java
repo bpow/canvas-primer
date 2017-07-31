10f8a0baf371d597215ae33fa6c05580a4bd3cfe
Jason Reilly <jdr0887@gmail.com>
2017-07-31 16:13:57 -0400

    cleaning

--- primer-modules/primer-module-genome-reference/src/main/java/org/renci/canvas/primer/gr/commands/CreateCombinedFastaFileCallable.java
+++ primer-modules/primer-module-genome-reference/src/main/java/org/renci/canvas/primer/gr/commands/CreateCombinedFastaFileCallable.java
@@ -3,7 +3,6 @@ package org.renci.canvas.primer.gr.commands;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
