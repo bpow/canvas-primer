ed49e2e47c3bd6980363389d6b1f3f340c2cff6e
Jason Reilly <jdr0887@gmail.com>
2017-07-31 16:14:05 -0400

    cleaning

--- primer-modules/primer-module-genome-reference/src/main/java/org/renci/canvas/primer/gr/commands/DownloadAndSerializeAction.java
+++ primer-modules/primer-module-genome-reference/src/main/java/org/renci/canvas/primer/gr/commands/DownloadAndSerializeAction.java
@@ -1,14 +1,9 @@
 package org.renci.canvas.primer.gr.commands;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.concurrent.Executors;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.karaf.shell.api.action.Action;
 import org.apache.karaf.shell.api.action.Command;
-import org.apache.karaf.shell.api.action.Option;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
