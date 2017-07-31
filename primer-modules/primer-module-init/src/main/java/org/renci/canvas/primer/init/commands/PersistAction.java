5570e15fe2555c9fe2bc577721a4c9bce86f0600
Jason Reilly <jdr0887@gmail.com>
2017-07-31 16:08:53 -0400

    removing lists

--- primer-modules/primer-module-init/src/main/java/org/renci/canvas/primer/init/commands/PersistAction.java
+++ primer-modules/primer-module-init/src/main/java/org/renci/canvas/primer/init/commands/PersistAction.java
@@ -36,15 +36,6 @@ public class PersistAction implements Action {
 
         // these should all become Java enums
 
-        logger.info("Loading GroupingTypes");
-        List<String> gtList = Arrays.asList("single", "order", "join");
-        List<GroupingType> groupingTypeList = canvasDAOBeanService.getGroupingTypeDAO().findAll();
-        for (String gt : gtList) {
-            if (!groupingTypeList.stream().filter(a -> a.getId().equals(gt)).findAny().isPresent()) {
-                GroupingType groupingType = new GroupingType(gt);
-                canvasDAOBeanService.getGroupingTypeDAO().save(groupingType);
-            }
-        }
 
         logger.info("Loading VariantTypes");
         List<String> vtList = Arrays.asList("snp", "ins", "del", "sub", "ref");
@@ -56,16 +47,6 @@ public class PersistAction implements Action {
             }
         }
 
-        logger.info("Loading LocationTypes");
-        List<String> ltList = Arrays.asList("UTR", "UTR-5", "UTR-3", "intron", "exon", "intergenic", "potential RNA-editing site",
-                "intron/exon boundary");
-        List<LocationType> locationTypeList = canvasDAOBeanService.getLocationTypeDAO().findAll();
-        for (String lt : ltList) {
-            if (!locationTypeList.stream().filter(a -> a.getId().equals(lt)).findAny().isPresent()) {
-                LocationType locationType = new LocationType(lt);
-                canvasDAOBeanService.getLocationTypeDAO().save(locationType);
-            }
-        }
 
         logger.info("Loading DiagnosticStatusTypes");
         List<String> dstList = Arrays.asList("VCF loading", "Annotating variants", "Updating frequency table", "Updating dx bins",
