fbbc610c7aab79d8d22b3b276d49116405848392
Jason Reilly <jdr0887@gmail.com>
2017-10-04 15:55:01 -0400

    namespaceversion is not in pk anymore

--- primer-modules/primer-module-gene-lists/src/main/java/org/renci/canvas/primer/genes/commands/GenerateGeneListSupportFilesAction.java
+++ primer-modules/primer-module-gene-lists/src/main/java/org/renci/canvas/primer/genes/commands/GenerateGeneListSupportFilesAction.java
@@ -119,7 +119,7 @@ public class GenerateGeneListSupportFilesAction implements Action {
 
                                     List<AnnotationGeneExternalId> filteredAnnotationGeneExternalIds = annotationGeneExternalIds.stream()
                                             .filter(b -> "refseq".equals(b.getId().getNamespace()) && diagnosticResultVersion
-                                                    .getRefseqVersion().toString().equals(b.getId().getNamespaceVer()))
+                                                    .getRefseqVersion().toString().equals(b.getNamespaceVersion()))
                                             .collect(Collectors.toList());
 
                                     if (CollectionUtils.isNotEmpty(filteredAnnotationGeneExternalIds)) {
